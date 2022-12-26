4ca5c6b28e5a17c72aeb15d9f67d1bf7d651ad21
demisequartet <kaizukasi551@gmail.com>
2022-12-26 15:47:31 +0900

    ⚗️ ボタンでクリックイベントを用いた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,24 @@ import { Inter } from "@next/font/google";
 import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+  });
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        Click
+      </a>
       <Main page="index" />
     </>
   );
